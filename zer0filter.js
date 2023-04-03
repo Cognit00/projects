9bd9134b5872099dcd8d8933353041c681a6a14a
Cognit00 <128618077+Cognit00@users.noreply.github.com>
2023-04-03 15:08:34 -0500

    Update zer0filter.js

--- zer0filter.js
+++ zer0filter.js
@@ -1,1 +1,4 @@
-alert("hi uh disabled for now because snitch :steamsad:"); alert("uh yes do not share next time !1"); if (prompt("yes") != "ok") {while (true){}};
+alert("hi uh disabled for now because snitch :steamsad:"); alert("uh yes do not share next time !1"); 
+if (prompt("yes") != "ok") {
+  while (true){}
+}
